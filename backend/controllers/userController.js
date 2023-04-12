dee10192e153e5f9263f36154f9af50db6d46c19
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-04-11 21:48:24 -0400

    Last commit of the night

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,26 +1,30 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 //route POST /api/users
 //access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.json({
     message: "Register User",
   });
-};
+});
 
 //route POST /api/users/login
 //access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({
     message: "Login User",
   });
-};
+});
 
 //route GET /api/users/me
 //access Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({
     message: "User data retrieved",
   });
-};
+});
 
 module.exports = {
   registerUser,
